12cae90a54b4ab61e50fd0549c3af40d8f8a708e
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-10-15 15:03:50 +0100

    Simplify file to test pipeline

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {checkWebGPU} from './helper';
-import {renderTriangle} from "./render";
-
-const message = checkWebGPU() ? "hello, triangle!" : "webgpu not supported!";
 
 ReactDOM.render(
     <React.StrictMode>
         <h1>{message}</h1>
-        <canvas id="canvas-webgpu" width="968" height="720"/>
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-renderTriangle().then(() => {});
