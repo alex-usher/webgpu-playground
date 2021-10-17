878b5ba6a47ae04adb0a775bfe51432cc853aee2
vncnt-wng <vwang00@gmail.com>
2021-10-17 14:31:26 +0100

    Adds the home page and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+  
+import HomePage from './pages/HomePage'
 import ShaderCanvas from './components/ShaderCanvas'
+
 // import {checkWebGPU} from './helper'
-import {renderTriangle} from "./render"
+//import {renderTriangle} from "./render"
 
 import "./assets/style.css"
 
@@ -11,10 +19,20 @@ import "./assets/style.css"
 ReactDOM.render(
     <React.StrictMode>
         <div id="body">
-            <ShaderCanvas />
+            <Router>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomePage />
+                    </Route>
+                    <Route path="/users">
+                        <ShaderCanvas />
+                    </Route>
+                </Switch>
+            </Router>
+            
         </div>
     </React.StrictMode>,
     document.getElementById('root')
 );
 
-renderTriangle().then(() => {})
+//renderTriangle().then(() => {})
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,55 @@
-const HomePage = () => {
-    return (
-        <div>
+import React from 'react'
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import CardActionArea from '@mui/material/CardActionArea'
+import CardContent from "@mui/material/CardContent"
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
 
-        </div>
+class HomePage extends React.Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         shaders: []
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     // TODO replace with api call to get shaders 
+    // }
+
+    render() {
+        return (
+            <Container> 
+                <Grid container spacing={2} style={{paddingTop: "100px"}}> 
+                    {/* this.state.shaders.map() */}
+                    <ShaderCard />
+                    <ShaderCard />
+                    <ShaderCard />
+                </Grid>
+            </Container>
+        )
+    }
+}
+
+
+const ShaderCard = () => {
+    return (
+        <Grid item xs={12} md={4}>
+            <Card>
+                <CardActionArea 
+                    style={{height: "200px", background:"Grey"}}
+                    component={Link} to="/shader"
+                >
+                    <CardContent>
+                        <Typography variant="h4" align="center">
+                            triangle 
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
     )
 }
 
