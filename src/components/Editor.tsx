7427b49ec3161b8bcf3149a8b774e99e6835459f
Anna Chen <anna.ycx@gmail.com>
2021-10-17 14:37:39 +0100

    add editor component

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,9 @@
-const Editor = (value: string, onChange: () => void) => (
+interface EditorProps {
+    value: string
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+}
+
+const Editor = ({value, onChange} : EditorProps) => (
     <textarea className="editor" value={value} onChange={onChange} />
 )
 
--- src/pages/CodeEditorPage.tsx
+++ src/pages/CodeEditorPage.tsx
@@ -1,5 +1,17 @@
-const CodeEditorPage = () => (
-    <div></div>
-)
+import { useState } from 'react'
+import Editor from '../components/Editor'
+
+const CodeEditorPage = () => {
+    const [code, setCode] = useState("")
+    function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        setCode(e.target.value)
+    }
+
+    return (
+        <div>
+            <Editor value={code} onChange={handleChange} />
+        </div>
+    )
+}
 
 export default CodeEditorPage
