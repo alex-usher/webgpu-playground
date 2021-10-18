8cc6cded74c70c91cd677679af784a68ab42eb36
Anna Chen <anna.ycx@gmail.com>
2021-10-18 18:49:54 +0100

    remove unused placeholder variable

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,14 +1,13 @@
 import '../assets/editor.css'
 
 interface EditorProps {
-    placeholder?: string
     value: string
     onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
-const Editor = ({placeholder, value, onChange} : EditorProps) => (
+const Editor = ({value, onChange} : EditorProps) => (
     <div className="editor-container"> 
-        <textarea className="editor" value={value} placeholder={placeholder} onChange={onChange}  />
+        <textarea className="editor" value={value} onChange={onChange}  />
     </div>
 )
 
