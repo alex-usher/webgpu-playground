2455c436da518e1a9ae41c2271607464dbd1dba8
Alex Usher <alex_usher@outlook.com>
2021-10-19 15:09:05 +0100

    Adds tests for the code editor link with the show code button

--- src/test/pages/codeEditorPage.test.tsx
+++ src/test/pages/codeEditorPage.test.tsx
@@ -2,15 +2,19 @@ const helper = require("../../helper")
 const shaders = require("../../render")
 import {render} from "@testing-library/react"
 import CodeEditorPage from "../../pages/CodeEditorPage"
-import {shaderTriangleFragment,shaderTriangleVertex} from "../../render"
+import {shaderTriangleFragment, shaderTriangleVertex} from "../../render"
 
-const renderCodeEditorPage = () => render(<CodeEditorPage defaultVertexCode={shaderTriangleVertex} defaultFragmentCode={shaderTriangleFragment}/>)
+const renderCodeEditorPage = () => render(<CodeEditorPage defaultVertexCode={shaderTriangleVertex}
+                                                          defaultFragmentCode={shaderTriangleFragment}/>)
 
 let checkWebGPUMock: jest.SpyInstance
 let simpleShaderMock: jest.SpyInstance
 
 // helper constants defining the button texts and ids
 const SHOW_CODE_ID = "show-code-button"
+const COMPILE_ID = "compile-button"
+const VERTEX_EDITOR_CLASS = "vertex-editor"
+const FRAGMENT_EDITOR_CLASS = "fragment-editor"
 const SHOW_CODE_TEXT = "View Code"
 const HIDE_CODE_TEXT = "Hide Code"
 
@@ -18,36 +22,71 @@ const doMocks = () => {
     checkWebGPUMock = jest.spyOn(helper, "checkWebGPU")
     checkWebGPUMock.mockReturnValue(true)
     simpleShaderMock = jest.spyOn(shaders, "renderSimpleShader")
-    simpleShaderMock.mockImplementation(() => {})
+    simpleShaderMock.mockImplementation(() => {
+    })
 }
 
-describe("Show Code Button Click Tests", () => {
-    beforeEach(doMocks)
+describe("Default render tests", () => {
+    beforeEach(() => {
+        doMocks()
+        renderCodeEditorPage()
+    })
 
     afterEach(jest.resetAllMocks)
 
-    it("Clicking the show code button changes its text", () => {
+    test("By default the fragment editor is not rendered", () => {
+        const fragmentEditorDiv = document.querySelector(`.${FRAGMENT_EDITOR_CLASS}`)
+        expect(fragmentEditorDiv?.hasChildNodes()).toBeFalsy()
+    })
+
+
+    test("By default the vertex editor is not rendered", () => {
+        const vertexEditorDiv = document.querySelector(`.${VERTEX_EDITOR_CLASS}`)
+        expect(vertexEditorDiv?.hasChildNodes()).toBeFalsy()
+    })
+})
+
+describe("Show Code Button Click Tests", () => {
+    let showCodeButton: HTMLElement | null
+
+    beforeEach(() => {
+        doMocks()
         renderCodeEditorPage()
+        showCodeButton = document.getElementById(SHOW_CODE_ID)
+    })
 
-        const showCodeButton = document.getElementById(SHOW_CODE_ID)
+    afterEach(() => {
+        jest.resetAllMocks()
+        showCodeButton = null
+    })
 
-        expect(showCodeButton).not.toBeNull()
+    test("Clicking the show code button changes its text", () => {
         expect(showCodeButton!.textContent).toEqual(SHOW_CODE_TEXT)
-
         showCodeButton!.click()
-
         expect(showCodeButton!.textContent).toEqual(HIDE_CODE_TEXT)
     })
 
-    it("Clicking the show code button alternates its text", () => {
-        renderCodeEditorPage()
-
-        const showCodeButton = document.getElementById(SHOW_CODE_ID)
-
-        expect(showCodeButton).not.toBeNull()
+    test("Clicking the show code button alternates its text", () => {
         expect(showCodeButton!.textContent).toEqual(SHOW_CODE_TEXT)
         showCodeButton!.click()
         showCodeButton!.click()
         expect(showCodeButton!.textContent).toEqual(SHOW_CODE_TEXT)
+    })
+
+    test("Clicking the show code button displays the compile button", () => {
+        showCodeButton!.click()
+        expect(document.getElementById(COMPILE_ID)).not.toBeNull()
+    })
+
+    test("Clicking the show code button displays the vertex editor", () => {
+        showCodeButton!.click()
+        const vertexEditorDiv = document.querySelector(`.${VERTEX_EDITOR_CLASS}`)
+        expect(vertexEditorDiv?.hasChildNodes()).toBeTruthy()
+    })
+
+    test("Clicking the show code button displays the fragment editor", () => {
+        showCodeButton!.click()
+        const fragmentEditorDiv = document.querySelector(`.${FRAGMENT_EDITOR_CLASS}`)
+        expect(fragmentEditorDiv?.hasChildNodes()).toBeTruthy()
     })
 })
