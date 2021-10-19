95399120122e20277427c7f7ce24124f974ec711
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-10-19 15:55:52 +0100

    [rg719, hw1119] Added save button

--- src/pages/CodeEditorPage.tsx
+++ src/pages/CodeEditorPage.tsx
@@ -1,17 +1,25 @@
 import Button from "@mui/material/Button";
-import Editor from '../components/Editor'
+import Editor from '../components/Editor';
 import ShaderCanvas from "../components/ShaderCanvas";
-import {useState} from 'react'
+import { useState } from 'react';
+import fs from 'fs';
 
 import "../assets/style.css";
-import "../assets/codeEditorPage.css"
+import "../assets/codeEditorPage.css";
 
 interface CodeEditorPageProps {
     defaultVertexCode: string
     defaultFragmentCode: string
 }
 
-const CodeEditorPage = ({defaultVertexCode, defaultFragmentCode}: CodeEditorPageProps) => {
+const saveShaderCode = (vertexCode: string, fragmentCode: string) => {
+
+    let data = "garbage";
+    fs.writeFileSync("smthn.txt", data);
+
+}
+
+const CodeEditorPage = ({ defaultVertexCode, defaultFragmentCode }: CodeEditorPageProps) => {
     const [vertexCode, setVertexCode] = useState(defaultVertexCode)
     const [fragmentCode, setFragmentCode] = useState(defaultFragmentCode)
     const [showCode, setShowCode] = useState(false)
@@ -27,15 +35,19 @@ const CodeEditorPage = ({defaultVertexCode, defaultFragmentCode}: CodeEditorPage
                     setViewCodeText(showCode ? "View Code" : "Hide Code")
                 }} color={"primary"}>{viewCodeText}</Button>
                 {showCode ? <Button id="compile-button" variant="outlined" disableElevation
-                 color="secondary" style={{margin: "0 0 0 1em"}} onClick={()=> {
-                     setRenderedVertexCode(vertexCode)
-                     setRenderedFragmentCode(fragmentCode)
-                     }}>Compile</Button> : <></>}
+                    color="secondary" style={{ margin: "0 0 0 1em" }} onClick={() => {
+                        setRenderedVertexCode(vertexCode)
+                        setRenderedFragmentCode(fragmentCode)
+                    }}>Compile</Button> : <></>}
+                {showCode ? <Button variant="outlined" disableElevation
+                    color="success" style={{ margin: "0 0 0 1em" }} onClick={() => {
+
+                    }}>Save</Button> : <></>}
             </div>
             <ShaderCanvas vertexCode={renderedVertexCode} fragmentCode={renderedFragmentCode} />
             <div className="editors">
                 <div className="vertex-editor">
-                    {showCode ? <Editor value={vertexCode} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {setVertexCode(e.target.value)}}/> : <></>}
+                    {showCode ? <Editor value={vertexCode} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => { setVertexCode(e.target.value) }} /> : <></>}
                 </div>
                 <div className="fragment-editor">
                     {showCode ? <Editor value={fragmentCode} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => { setFragmentCode(e.target.value) }} /> : <></>}
