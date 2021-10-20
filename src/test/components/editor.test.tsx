2297b3acb68fda087a639670ecb44efcb227843d
Alex Usher <57226865+alex-usher@users.noreply.github.com>
2021-10-20 10:06:00 +0100

    Adds a testsuite for frontend components and pages (#9)
    Adds a testsuite for frontend components and pages

---
+++ src/test/components/editor.test.tsx
@@ -0,0 +1,13 @@
+import {render} from '@testing-library/react'
+import Editor from "../../components/Editor"
+
+const testValue = "Test Value"
+const renderEditor = () => render(<Editor value={testValue} onChange={() => {}}/>)
+
+describe("Editor Component Tests", () => {
+    it("Should initialise the text area with the given value", () => {
+        const container = renderEditor().container.firstChild;
+
+        expect(container?.firstChild).toHaveProperty("value", testValue)
+    })
+})
---
+++ src/test/components/shaderCanvas.test.tsx
@@ -0,0 +1,46 @@
+const helper = require("../../helper")
+const shaders = require("../../render")
+import {render} from "@testing-library/react";
+import ShaderCanvas from "../../components/ShaderCanvas";
+import {shaderTriangleFragment, shaderTriangleVertex} from "../sample_shaders/triangle";
+
+const renderShaderCanvas = () =>
+    render(<ShaderCanvas vertexCode={shaderTriangleVertex} fragmentCode={shaderTriangleFragment}/>)
+
+let checkWebGPUMock: jest.SpyInstance
+let simpleShaderMock: jest.SpyInstance
+describe("Shader Canvas component tests", () => {
+    beforeEach(() => {
+        checkWebGPUMock = jest.spyOn(helper, "checkWebGPU")
+        simpleShaderMock = jest.spyOn(shaders, "renderSimpleShader")
+        simpleShaderMock.mockImplementation(() => {})
+    })
+
+    afterEach(() => {
+        jest.resetAllMocks()
+    })
+
+    it("Should not render the canvas when WebGPU is disabled", async () => {
+        checkWebGPUMock.mockReturnValue(false)
+
+        renderShaderCanvas()
+
+        expect(document.getElementById('canvas-webgpu')).toBeNull()
+    })
+
+    it("Should render the canvas when WebGPU is enabled", async () => {
+        checkWebGPUMock.mockReturnValue(true)
+
+        renderShaderCanvas()
+
+        expect(document.getElementById('canvas-webgpu')).not.toBeNull()
+    })
+
+    it("Should make calls to renderSimpleShader to render onto the canvas", () => {
+        checkWebGPUMock.mockReturnValue(true)
+
+        renderShaderCanvas()
+
+        expect(simpleShaderMock).toHaveBeenCalledTimes(1)
+    })
+})
