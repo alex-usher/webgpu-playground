3600fb07b413285bae2b8ff3ec9cf3a125945650
Hana Watson <60738180+hanawatson@users.noreply.github.com>
2021-10-20 13:01:10 +0100

    Firebase setup (#11)
    * [rg719, hw1119] Setup firebase config file and upload of public shaders
    
    * [rg719, hw1119] Added save button
    
    * [rg719, hw1119] Upload code to storage bucket and add link from public shader in database
    
    * [rg719, hw1119] Setup firebase config file and upload of public shaders
    
    * [rg719, hw1119] Added save button
    
    * [rg719, hw1119] Upload code to storage bucket and add link from public shader in database
    
    * [hw1119] rebase onto main, provide save button id
    
    * [hw1119] update package-lock for newer npm version
    
    * [hw1119] fix npm jest dependency errors
    
    * Rebuild package-lock.json to fix local errors
    
    Co-authored-by: Rohan Gupta <guptarohan@hotmail.co.uk>

--- src/components/ShaderCanvas.tsx
+++ src/components/ShaderCanvas.tsx
@@ -2,23 +2,23 @@ import { useEffect } from "react"
 import "../assets/shaderCanvas.css"
 import { checkWebGPU } from '../helper'
 import { renderSimpleShader } from "../render"
-import Typography from "@mui/material/Typography"
+import Typography from "@mui/material/Typography";
 
 interface ShaderCanvasInput {
     vertexCode: string
     fragmentCode: string
 }
 
-const ShaderCanvas = ({vertexCode, fragmentCode}: ShaderCanvasInput) => {
+const ShaderCanvas = ({ vertexCode, fragmentCode }: ShaderCanvasInput) => {
     useEffect(() => {
         renderSimpleShader(vertexCode, fragmentCode)
     }, [vertexCode, fragmentCode])
 
     return (
-        <div style={{color: "white", height: "90%"}}>
+        <div style={{ color: "white", height: "90%" }}>
             {checkWebGPU()
                 ? <div className="canvas-container">
-                    <canvas id="canvas-webgpu" width="968" height="720"/>
+                    <canvas id="canvas-webgpu" width="968" height="720" />
                 </div>
                 : <Typography variant="h2"> webgpu not supported!</Typography>}
         </div>
--- src/pages/CodeEditorPage.tsx
+++ src/pages/CodeEditorPage.tsx
@@ -1,17 +1,38 @@
 import Button from "@mui/material/Button";
-import Editor from '../components/Editor'
+import Editor from '../components/Editor';
 import ShaderCanvas from "../components/ShaderCanvas";
-import {useState} from 'react'
+import { useState } from 'react';
+import { ref, uploadString } from "firebase/storage";
+import { firestorage, firedb } from "../firebase"
+import { collection, addDoc } from "firebase/firestore"
+import { v4 as uuidv4 } from "uuid";
 
 import "../assets/style.css";
-import "../assets/codeEditorPage.css"
+import "../assets/codeEditorPage.css";
 
 interface CodeEditorPageProps {
     defaultVertexCode: string
     defaultFragmentCode: string
 }
 
-const CodeEditorPage = ({defaultVertexCode, defaultFragmentCode}: CodeEditorPageProps) => {
+const saveShaderCode = (vertexCode: string, fragmentCode: string, shaderName: string) => {
+    const vertexFile = uuidv4() + shaderName + "_vertex.txt"
+    const fragmentFile = uuidv4() + shaderName + "_fragment.txt"
+
+    const vertexRef = ref(firestorage, vertexFile);
+    const fragmentRef = ref(firestorage, fragmentFile);
+
+    uploadString(vertexRef, vertexCode)
+    uploadString(fragmentRef, fragmentCode)
+
+    addDoc(collection(firedb, "public-shaders"), {
+        shader_name: shaderName,
+        vertex_code: vertexFile,
+        fragment_code: fragmentFile,
+    });
+}
+
+const CodeEditorPage = ({ defaultVertexCode, defaultFragmentCode }: CodeEditorPageProps) => {
     const [vertexCode, setVertexCode] = useState(defaultVertexCode)
     const [fragmentCode, setFragmentCode] = useState(defaultFragmentCode)
     const [showCode, setShowCode] = useState(false)
@@ -27,21 +48,25 @@ const CodeEditorPage = ({defaultVertexCode, defaultFragmentCode}: CodeEditorPage
                     setViewCodeText(showCode ? "View Code" : "Hide Code")
                 }} color={"primary"}>{viewCodeText}</Button>
                 {showCode ? <Button id="compile-button" variant="outlined" disableElevation
-                 color="secondary" style={{margin: "0 0 0 1em"}} onClick={()=> {
-                     setRenderedVertexCode(vertexCode)
-                     setRenderedFragmentCode(fragmentCode)
-                     }}>Compile</Button> : <></>}
+                    color="secondary" style={{ margin: "0 0 0 1em" }} onClick={() => {
+                        setRenderedVertexCode(vertexCode)
+                        setRenderedFragmentCode(fragmentCode)
+                    }}>Compile</Button> : <></>}
+                {showCode ? <Button id="save-button" variant="outlined" disableElevation
+                    color="success" style={{ margin: "0 0 0 1em" }} onClick={() => {
+                        saveShaderCode(vertexCode, fragmentCode, "shader name")
+                    }}>Save</Button> : <></>}
             </div>
             <ShaderCanvas vertexCode={renderedVertexCode} fragmentCode={renderedFragmentCode} />
             <div className="editors">
                 <div className="vertex-editor">
-                    {showCode ? <Editor value={vertexCode} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {setVertexCode(e.target.value)}}/> : <></>}
+                    {showCode ? <Editor value={vertexCode} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => { setVertexCode(e.target.value) }} /> : <></>}
                 </div>
                 <div className="fragment-editor">
                     {showCode ? <Editor value={fragmentCode} onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => { setFragmentCode(e.target.value) }} /> : <></>}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
