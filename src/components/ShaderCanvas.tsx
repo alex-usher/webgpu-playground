a723d149a3f1499d030adfb6a03cb5a2b74095b5
Alex Usher <57226865+alex-usher@users.noreply.github.com>
2021-10-21 12:28:03 +0100

    Responsive Canvas Scaling (#12)
    * Adjusts canvas styling to make it responsive
    
    * adjusts canvas aspect ratio calculation
    
    Co-authored-by: Alex Usher <alex_usher@outlook.com>

--- src/components/ShaderCanvas.tsx
+++ src/components/ShaderCanvas.tsx
@@ -4,6 +4,9 @@ import { checkWebGPU } from '../helper'
 import { renderSimpleShader } from "../render"
 import Typography from "@mui/material/Typography";
 
+const WIDTH_ASPECT = 968
+const HEIGHT_ASPECT = 720
+
 interface ShaderCanvasInput {
     vertexCode: string
     fragmentCode: string
@@ -14,11 +17,13 @@ const ShaderCanvas = ({ vertexCode, fragmentCode }: ShaderCanvasInput) => {
         renderSimpleShader(vertexCode, fragmentCode)
     }, [vertexCode, fragmentCode])
 
+    const aspectMultiple = Math.min(window.innerWidth / WIDTH_ASPECT, window.innerHeight / HEIGHT_ASPECT)
+
     return (
         <div style={{ color: "white", height: "90%" }}>
             {checkWebGPU()
                 ? <div className="canvas-container">
-                    <canvas id="canvas-webgpu" width="968" height="720" />
+                    <canvas id="canvas-webgpu" width={aspectMultiple * WIDTH_ASPECT} height={aspectMultiple * HEIGHT_ASPECT} />
                 </div>
                 : <Typography variant="h2"> webgpu not supported!</Typography>}
         </div>
