a8ae322016893fce90532b6546f7b21c9ef0efd1
vncnt-wng <vwang00@gmail.com>
2021-10-23 05:08:48 +0100

    Adds scrolling line numbers to the code editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync';
 import '../assets/editor.css'
 
 interface EditorProps {
@@ -5,10 +7,64 @@ interface EditorProps {
     onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
-const Editor = ({value, onChange} : EditorProps) => (
-    <div className="editor-container"> 
-        <textarea className="editor" value={value} onChange={onChange}  />
-    </div>
-)
+
+
+const Editor = ({value, onChange} : EditorProps) => {
+    
+    const lines = (value.split(/\r\n|\r|\n/)||[]).length + 1
+
+        
+    const gutterRef = React.useRef<HTMLDivElement>(null)
+    const editorRef = React.useRef<HTMLTextAreaElement>(null)
+
+
+
+    const lineNumbers = [...Array(lines)].map(
+        (_, i) => (
+            <div key = {i}>
+            {i + 1}<br/>
+            </div>
+        )
+    )
+
+    // Set the gutter scroll position to the editor position
+    const textAreaScroll = (e: React.UIEvent<HTMLElement>): void => {
+        if (gutterRef.current! && editorRef.current!) {
+            editorRef.current.focus()
+            gutterRef.current.focus()
+            gutterRef.current.scrollTop = editorRef.current.scrollTop
+        }
+    }
+
+
+
+    return (
+        <ScrollSync>
+        <div className="editor-container"> 
+            
+                <ScrollSyncPane>
+                    <div className="editor-gutter-container">
+                        <div 
+                            className="editor-gutter scroll-text-style"
+                            ref={gutterRef as React.RefObject<HTMLDivElement>}
+                        >
+                            {lineNumbers}
+                        </div>
+                    </div>
+                </ScrollSyncPane>
+                <ScrollSyncPane>
+                    <textarea 
+                        className="editor scroll-text-style" 
+                        value={value} 
+                        onChange={onChange}  
+                        onScroll={textAreaScroll}
+                        ref={editorRef as React.RefObject<HTMLTextAreaElement>}
+                    />
+                </ScrollSyncPane>
+           
+        </div>
+        </ScrollSync>
+    )
+}
 
 export default Editor
