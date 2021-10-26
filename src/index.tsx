274385db0c24153dfc15b2ceb6db03951762cb76
Anna Chen <anna.ycx@gmail.com>
2021-10-26 22:35:36 +0100

    adds snackbar

--- src/index.tsx
+++ src/index.tsx
@@ -5,28 +5,30 @@ import {
     Switch,
     Route
 } from "react-router-dom";
-  
+
 import HomePage from './pages/HomePage'
 import CodeEditorPage from './pages/CodeEditorPage'
+import { SnackbarProvider } from 'notistack'
 
 import "./assets/style.css"
 import { rectangleFragment, rectangleVertex/*, shaderTriangleFragment, shaderTriangleVertex*/ } from './render';
 
 ReactDOM.render(
     <React.StrictMode>
-        <div id="body">
-            <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        <HomePage />
-                    </Route>
-                    <Route path="/editor">
-                        <CodeEditorPage defaultVertexCode={rectangleVertex} defaultFragmentCode={rectangleFragment} />
-                    </Route>
-                </Switch>
-            </Router>
-            
-        </div>
+        <SnackbarProvider maxSnack={1}>
+            <div id="body">
+                <Router>
+                    <Switch>
+                        <Route path="/" exact>
+                            <HomePage />
+                        </Route>
+                        <Route path="/editor">
+                            <CodeEditorPage defaultVertexCode={rectangleVertex} defaultFragmentCode={rectangleFragment} />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        </SnackbarProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,83 +1,63 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
 import CardActionArea from '@mui/material/CardActionArea'
 import CardContent from "@mui/material/CardContent"
 import CardMedia from "@mui/material/CardMedia"
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import { Link } from "react-router-dom";
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import { Link } from "react-router-dom"
 
 import "../assets/homePage.css"
 
-class HomePage extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         shaders: []
-    //     }
-    // }
+const HomePage = () => (
+    <Container>
+        <Grid container spacing={2} style={{ paddingTop: "100px" }} alignItems="center" justifyContent="flex-end">
+            <Grid
+                item
+                container
+                justifyContent="space-between"
+                alignItems="center"
+                spacing={2}
+                className="title-header"
+            >
 
-    // componentDidMount() {
-    //     // TODO replace with api call to get shaders 
-    // }
-
-    render() {
-        return (
-            <Container> 
-                <Grid container spacing={2} style={{paddingTop: "100px"}} alignItems="center" justifyContent="flex-end"> 
-                    
-                    <Grid 
-                        item 
-                        container 
-                        justifyContent="space-between" 
-                        alignItems="center" 
-                        spacing={2} 
-                        className="title-header"
-                    >
-                    
-                        <Grid item>
-                            <Typography variant="h3">
-                                WebGPU Playground
-                            </Typography>
-                        </Grid>
-                        <Grid item >
-                                <Button variant="outlined" disableElevation component={Link} to="/editor"> 
-                                    New Shader Sandbox
-                                </Button>
-                        </Grid>
-                    </Grid>
-                    {/* this.state.shaders.map() */}
-                    <ShaderCard />
-                    <ShaderCard />
-                    <ShaderCard />
-                </Grid>
-            </Container>
-        )
-    }
-}
- 
-
-const ShaderCard = () => {
-    return (
-        <Grid item xs={12} sm={6} md={4}>
-            <Card variant="outlined">
-                <CardActionArea component={Link} to="/editor" className="shader-card" style={{height:"20%", background:"#4F5358",color:"white"}}>
-                    <CardMedia
-                        className="shader-card-image"
-                        component="img"
-                        src="https://i.ibb.co/M5Z06wy/triangle.png"
-                    />
-                    <CardContent>
-                        <Typography variant="h4" align="left">
-                            Triangle 
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
+            <Grid item>
+                <Typography variant="h3">
+                    WebGPU Playground
+                </Typography>
+            </Grid>
+            <Grid item >
+                <Button variant="outlined" disableElevation component={Link} to="/editor">
+                    New Shader Sandbox
+                </Button>
+            </Grid>
         </Grid>
-    )
-}
+            {/* this.state.shaders.map() */}
+            <ShaderCard />
+            <ShaderCard />
+            <ShaderCard />
+        </Grid>
+    </Container>
+)
+
+const ShaderCard = () => (
+    <Grid item xs={12} sm={6} md={4}>
+        <Card variant="outlined">
+            <CardActionArea component={Link} to="/editor" className="shader-card" style={{ height: "20%", background: "#4F5358", color: "white" }}>
+                <CardMedia
+                    className="shader-card-image"
+                    component="img"
+                    src="https://i.ibb.co/M5Z06wy/triangle.png"
+                />
+                <CardContent>
+                    <Typography variant="h4" align="left">
+                        Triangle
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    </Grid>
+)
 
 export default HomePage
