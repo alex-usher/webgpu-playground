68bf5af124c7a1597ed5bbb9b47e17ed3e336df0
Alex Usher <alex_usher@outlook.com>
2021-10-27 10:05:17 +0100

    adds import statement for useEffect

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ScrollSync, ScrollSyncPane} from 'react-scroll-sync';
 import '../assets/editor.css'
 import Prism from "prismjs"
