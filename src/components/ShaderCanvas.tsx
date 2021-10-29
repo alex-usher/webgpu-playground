6dcc9233b7044a6e9a2b1c0e582445a721b30a30
Sophie Elliott <85115371+sophiebella28@users.noreply.github.com>
2021-10-29 22:35:50 +0100

    Mouse Position in wgsl editor (#21)
    * [av1019, sce19] Add event listener to get mouse coordinates from canvas
    
    * [sce19] fixes compile errors with e type and renderTriangle
    
    * [av1019, sce19] Fix mouse coordinates bug
    
    * [sce19] adds function to query mouse position and changes wgsl code to test it more efficiently
    
    * [sce19] removes event listener so mouse moves successfully, needs to access canvas offset to get accurate mouse position
    
    * [sce19][av1019] got canvas position and scaled by height ratio and made it work
    
    * [sce19][av1019] removes print statements
    
    * fixes package-lock issues
    
    * retrying npm version fix
    
    * Add aspectMultiple to hook dependency
    
    * Updated style, remove semicolons
    
    Co-authored-by: Apoorva Verma <av1019@ic.ac.uk>
    Co-authored-by: Apoorva Verma <apoorva.verma@hotmail.com>

--- src/components/ShaderCanvas.tsx
+++ src/components/ShaderCanvas.tsx
@@ -1,33 +1,63 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "../assets/shaderCanvas.css"
-import { checkWebGPU } from '../helper'
-import {renderShader} from "../render"
-import Typography from "@mui/material/Typography";
+import { checkWebGPU } from "../helper"
+import { renderShader, updateCoordinates } from "../render"
+import Typography from "@mui/material/Typography"
 
 const WIDTH_ASPECT = 968
 const HEIGHT_ASPECT = 720
+const ASPECT_RATIO = 0.9
 
 interface ShaderCanvasInput {
-    vertexCode: string
-    fragmentCode: string
+  vertexCode: string
+  fragmentCode: string
 }
 
 const ShaderCanvas = ({ vertexCode, fragmentCode }: ShaderCanvasInput) => {
-    useEffect(() => {
-        renderShader(vertexCode, fragmentCode)
-    }, [vertexCode, fragmentCode])
 
-    const aspectMultiple = Math.min(window.innerWidth / WIDTH_ASPECT, window.innerHeight / HEIGHT_ASPECT)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
-    return (
-        <div style={{ color: "white", height: "90%" }}>
-            {checkWebGPU()
-                ? <div className="canvas-container">
-                    <canvas id="canvas-webgpu" width={aspectMultiple * WIDTH_ASPECT} height={aspectMultiple * HEIGHT_ASPECT} />
-                </div>
-                : <Typography variant="h2"> webgpu not supported!</Typography>}
+  const aspectMultiple = Math.min(
+    window.innerWidth / WIDTH_ASPECT,
+    window.innerHeight / HEIGHT_ASPECT
+  );
+
+  useEffect(() => {
+    renderShader(vertexCode, fragmentCode)
+  }, [vertexCode, fragmentCode])
+
+  useEffect(() => {
+    const setFromEvent = (e: MouseEvent) => {
+      const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement
+      let offsetLeft = canvas.getBoundingClientRect().left
+      let offsetTop = canvas.getBoundingClientRect().top
+      let xCoord = (e.pageX - offsetLeft) / (ASPECT_RATIO * aspectMultiple * WIDTH_ASPECT) * 2 - 1
+      let yCoord = -((e.pageY - offsetTop) / (ASPECT_RATIO * aspectMultiple * HEIGHT_ASPECT) * 2 - 1)
+      setPosition({ x: xCoord, y: yCoord})
+      updateCoordinates(position)
+    };
+    window.addEventListener("mousemove", setFromEvent)
+
+    return () => {
+      window.removeEventListener("mousemove", setFromEvent)
+    };
+  }, [position, aspectMultiple])
+
+  return (
+    <div style={{ color: "white", height: "90%" }}>
+      {checkWebGPU() ? (
+        <div className="canvas-container">
+          <canvas
+            id="canvas-webgpu"
+            width={aspectMultiple * WIDTH_ASPECT}
+            height={aspectMultiple * HEIGHT_ASPECT}
+          />
         </div>
-    )
+      ) : (
+        <Typography variant="h2"> webgpu not supported!</Typography>
+      )}
+    </div>
+  )
 }
 
-export default ShaderCanvas;
+export default ShaderCanvas
--- src/render.ts
+++ src/render.ts
@@ -1,4 +1,4 @@
-import {checkWebGPU} from "./helper";
+import { checkWebGPU } from "./helper"
 
 export const rectangleVertex = `struct VertexInput {
     [[location(0)]] position: vec2<f32>;
@@ -13,6 +13,8 @@ struct VertexOutput {
 [[block]]
 struct ViewParams {
     time: f32;
+    x: f32;
+    y: f32;
 };
 
 [[group(0), binding(0)]]
@@ -22,7 +24,7 @@ var<uniform> view_params: ViewParams;
 fn main([[builtin(vertex_index)]] index: u32, vert: VertexInput) -> VertexOutput {
     var pos = array<vec2<f32>, 6>(
         vec2<f32>(1.0, 1.0),
-        vec2<f32>(1.0, -1.0),
+        vec2<f32>(view_params.x, view_params.y),
         vec2<f32>(-1.0, -1.0),
         vec2<f32>(1.0, 1.0),
         vec2<f32>(-1.0, 1.0),
@@ -39,7 +41,7 @@ fn main([[builtin(vertex_index)]] index: u32, vert: VertexInput) -> VertexOutput
     );
 
     var out: VertexOutput;
-    out.position = vec4<f32>(pos[index], 0.0, 1.0) + vec4<f32>(sin(view_params.time * 0.01), cos(view_params.time * 0.01), 0.0, 0.0);
+    out.position = vec4<f32>(pos[index], 0.0, 1.0);
     out.color = vert.color + vec4<f32>(color[index], 1.0);
     return out;
 };`
@@ -102,6 +104,15 @@ const outputMessages = async (shaderModule: GPUShaderModule) => {
     return false
 }
 
+let x = 0
+let y = 0
+
+export const updateCoordinates = (position: { x: number, y: number }) => {
+    x = position.x
+    y = position.y
+}
+
+
 export const renderShader = async (vertex: string, fragment: string) => {
     if (!checkWebGPU()) {
         return
@@ -115,8 +126,8 @@ export const renderShader = async (vertex: string, fragment: string) => {
     const stateFormat = 'bgra8unorm'
     const depthFormat = 'depth24plus-stencil8'
 
-    const vertexShaderModule = device.createShaderModule({code: vertex})
-    const fragmentShaderModule = device.createShaderModule({code: fragment})
+    const vertexShaderModule = device.createShaderModule({ code: vertex })
+    const fragmentShaderModule = device.createShaderModule({ code: fragment })
 
     // check for compilation failures and output any compile messages
     if (!(await outputMessages(vertexShaderModule) && await outputMessages(fragmentShaderModule))) {
@@ -152,10 +163,10 @@ export const renderShader = async (vertex: string, fragment: string) => {
 
     dataBuffer.unmap()
 
-    context.configure({device: device, format: stateFormat, usage: GPUTextureUsage.RENDER_ATTACHMENT})
+    context.configure({ device: device, format: stateFormat, usage: GPUTextureUsage.RENDER_ATTACHMENT })
 
     const depthTexture = device.createTexture({
-        size: {width: canvas.width, height: canvas.height},
+        size: { width: canvas.width, height: canvas.height },
         format: depthFormat,
         usage: GPUTextureUsage.RENDER_ATTACHMENT
     })
@@ -164,11 +175,11 @@ export const renderShader = async (vertex: string, fragment: string) => {
         entries: [{
             binding: 0,
             visibility: GPUShaderStage.VERTEX,
-            buffer: [{type: "uniform"}]
+            buffer: [{ type: "uniform" }]
         } as GPUBindGroupLayoutEntry]
     })
 
-    const layout = device.createPipelineLayout({bindGroupLayouts: [bindGroupLayout]})
+    const layout = device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout] })
 
     const renderPipeline = device.createRenderPipeline({
         layout: layout,
@@ -179,40 +190,54 @@ export const renderShader = async (vertex: string, fragment: string) => {
                 arrayStride: 6 * 4,
                 stepMode: "vertex",
                 attributes: [
-                    {format: "float32x4", offset: 0, shaderLocation: 0},
-                    {format: "float32x4", offset: 2 * 4, shaderLocation: 1}
+                    { format: "float32x4", offset: 0, shaderLocation: 0 },
+                    { format: "float32x4", offset: 2 * 4, shaderLocation: 1 }
                 ]
             }]
         },
         fragment: {
             module: fragmentShaderModule,
             entryPoint: "main",
-            targets: [{format: stateFormat}]
+            targets: [{ format: stateFormat }]
         },
-        depthStencil: {format: depthFormat, depthWriteEnabled: true, depthCompare: "less"}
+        depthStencil: { format: depthFormat, depthWriteEnabled: true, depthCompare: "less" }
     })
 
     const viewParamsBuffer = device.createBuffer({
-        size: 4,
+        size: 4 * 3,
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     })
 
     const viewParamsBindGroup = device.createBindGroup({
         layout: bindGroupLayout,
-        entries: [{binding: 0, resource: {buffer: viewParamsBuffer}}]
+        entries: [{ binding: 0, resource: { buffer: viewParamsBuffer } }]
     })
 
     // track when canvas is visible and only render when true
     let canvasVisible = false
     const observer = new IntersectionObserver(e => {
-        canvasVisible = e[0].isIntersecting;
-    }, {threshold: [0]})
+        canvasVisible = e[0].isIntersecting
+    }, { threshold: [0] })
     observer.observe(canvas)
 
-    let time = 0;
+    let time = 0
     const frame = () => {
+
+
         if (canvasVisible) {
             const upload = device.createBuffer({
+                size: 4,
+                usage: GPUBufferUsage.COPY_SRC,
+                mappedAtCreation: true
+            })
+
+            const xBuffer = device.createBuffer({
+                size: 4,
+                usage: GPUBufferUsage.COPY_SRC,
+                mappedAtCreation: true
+            })
+
+            const yBuffer = device.createBuffer({
                 size: 4,
                 usage: GPUBufferUsage.COPY_SRC,
                 mappedAtCreation: true
@@ -221,8 +246,14 @@ export const renderShader = async (vertex: string, fragment: string) => {
             new Float32Array(upload.getMappedRange()).set([time])
             upload.unmap()
 
+            new Float32Array(xBuffer.getMappedRange()).set([x])
+            xBuffer.unmap()
+
+            new Float32Array(yBuffer.getMappedRange()).set([y])
+            yBuffer.unmap()
+
             const renderPassDescription = {
-                colorAttachments: [{view: context.getCurrentTexture().createView(), loadValue: [0.0, 0.0, 0.0, 0.0]}],
+                colorAttachments: [{ view: context.getCurrentTexture().createView(), loadValue: [0.0, 0.0, 0.0, 0.0] }],
                 depthStencilAttachment: {
                     view: depthTexture.createView(),
                     depthLoadValue: 1.0,
@@ -234,6 +265,8 @@ export const renderShader = async (vertex: string, fragment: string) => {
 
             const commandEncoder = device.createCommandEncoder()
             commandEncoder.copyBufferToBuffer(upload, 0, viewParamsBuffer, 0, 4)
+            commandEncoder.copyBufferToBuffer(xBuffer, 0, viewParamsBuffer, 4, 4)
+            commandEncoder.copyBufferToBuffer(yBuffer, 0, viewParamsBuffer, 8, 4)
 
             const renderPass = commandEncoder.beginRenderPass(renderPassDescription as GPURenderPassDescriptor)
             renderPass.setPipeline(renderPipeline)
@@ -244,8 +277,8 @@ export const renderShader = async (vertex: string, fragment: string) => {
             renderPass.endPass()
             device.queue.submit([commandEncoder.finish()])
             time++
-        }
 
+        }
         requestAnimationFrame(frame)
     }
 
