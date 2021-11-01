22bdaac4d455ef0c5bda49cf79dcdc1a1d643a54
sce19 <sce19@ic.ac.uk>
2021-11-01 17:49:39 +0000

    cleans up code and reformats module

--- src/components/CardCarousel.tsx
+++ src/components/CardCarousel.tsx
@@ -8,39 +8,18 @@ interface CarouselProps {
     shaderList: Shader[]
 }
 
-enum Direction {
-    Left,
-    Right,
-  }
-
 export const CardCarousel = ({ sectionName, shaderList }: CarouselProps) => {
-    function handleNav(direction: Direction) {
-        switch(direction) {
-            case Direction.Left: {
-                break;
-            }
-            case Direction.Right: {
-                break;
-            }
-            default: {
-                throw new TypeError(); 
-                // It's a type error because it should be impossible
-            }
-        }
-    }
-
-
     return (
         <div className="row" >
             {/* title */}
-            <Typography variant="h2">
+            <Typography variant="h2" className="title">
                 {sectionName}
             </Typography>
             {/* container -> posters */}
             <div className="row__blocks">
                 {/* several row posters */}
                 {shaderList.map((shader) => (
-                   <li className="row__poster row__posterLarge"><ShaderCard shader={shader}/> </li>
+                   <li className="row__tile "><ShaderCard shader={shader}/> </li>
                 ))}
             </div>
       </div >)
