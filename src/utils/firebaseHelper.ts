06b474933a77ea1b4fbe5ea13d80dec3bd57bda8
Alex Usher <alex_usher@outlook.com>
2021-11-02 15:53:34 +0000

    Adds default implementations for firebase interface

---
+++ src/utils/firebaseHelper.ts
@@ -0,0 +1,13 @@
+import { Shader } from "../objects/Shader";
+
+export const getPublicShaders = (): Shader[] => {
+  return [];
+};
+
+export const getUserPublicShaders = (): Shader[] => {
+  return [];
+};
+
+export const getUserPrivateShaders = (): Shader[] => {
+  return [];
+};
