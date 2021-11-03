47a9630654c95c9c86663daec85bb4668236d4f7
Hana Watson <hannah@tnsc.co.uk>
2021-11-03 19:17:12 +0000

    comment out new shader name entry box to comply with tests in the interest of prioritising merge

--- src/test/pages/codeEditorPage.test.tsx
+++ src/test/pages/codeEditorPage.test.tsx
@@ -118,10 +118,9 @@ describe("Button Click Tests", () => {
     expect(vertexEditorDiv?.hasChildNodes()).toBeTruthy();
 
     const textAreas: HTMLElement[] = screen.getAllByRole("textbox");
-    expect(textAreas.length).toBe(3);
+    expect(textAreas.length).toBe(2);
     expect(textAreas[0]).toBeInTheDocument();
     expect(textAreas[1]).toBeInTheDocument();
-    expect(textAreas[2]).toBeInTheDocument();
   });
 
   test("Clicking the compile code button results in calling the WebGPU render function", () => {
@@ -146,8 +145,8 @@ describe("Code editor tests", () => {
 
     document.getElementById(SHOW_CODE_ID)?.click();
     const textAreas: HTMLElement[] = screen.getAllByRole("textbox");
-    vertexEditor = textAreas[1];
-    fragmentEditor = textAreas[2];
+    vertexEditor = textAreas[0];
+    fragmentEditor = textAreas[1];
   });
 
   afterEach(() => {
