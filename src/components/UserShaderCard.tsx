b7a21e906cced8776d8fad0249cc33ced6d75c29
Hana Watson <hannah@tnsc.co.uk>
2021-12-31 17:07:01 +0000

    fix typo in public warning

--- src/components/UserShaderCard.tsx
+++ src/components/UserShaderCard.tsx
@@ -79,7 +79,7 @@ const PublicWarningDialog = ({
       <DialogContent style={{ display: "flex", justifyContent: "center" }}>
         <DialogContentText>
           Publishing your shader will allow other users to save, edit, and
-          publically reupload it under their name. You may specify an alternate
+          publicly reupload it under their name. You may specify an alternate
           license for your shader, but WebGPUniverse is not responsible for
           upholding the terms of said license.
           <br />
